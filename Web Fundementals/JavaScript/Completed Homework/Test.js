5297525efcc50745f27c3fdea9a246f3bfc9676d
Scott Eskridge <scott.eskridge@gmail.com>
2017-03-04 11:33:56 -0800

    3_3_17 update

---
+++ Web Fundementals/JavaScript/Completed Homework/Test.js
@@ -0,0 +1,20 @@
+<script>
+
+
+  var one = 1;
+  var two = 2;
+  var three = 3;
+  var four = 4;
+
+  var me = "scott";
+  var myself ="scoot";
+  var andI = "scotty";
+
+  var yes = true;
+  var no = false;
+
+  var
+
+  console.log(one,two,three,four,me,myself,andI,yes,no);
+
+</script>

eb120cb33c8927859bcecde88c4a12c72ba96ec0
Scott Eskridge <scott.eskridge@gmail.com>
2017-03-13 00:08:58 -0700

    the wall

--- SQL/The Wall/server.py
+++ SQL/The Wall/server.py
@@ -21,5 +21,30 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+@app.route('/confirmregistration', methods=["POST"])
+def confirmregistration():
+    return redirect('/')
+
+@app.route('/confirmlogin', methods=["POST"])
+def confirmlogin():
+    return redirect('/')
+
+@app.route('/postmessage', methods=["POST"])
+def postmessage():
+    return redirect('/')
+
+@app.route('/postcomment', methods=["POST"])
+def postcomment():
+    return redirect('/')
+
+@app.route('/logout', methods=["GET"])
+def logout():
+    session.clear()
+    return redirect('/')
+
 """end"""
 app.run(debug = True)

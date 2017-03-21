3e56dd13c2addc5d1c4ea29bb96417b9bf715901
Scott Eskridge <scott.eskridge@gmail.com>
2017-03-20 17:10:09 -0700

    bat file update

--- Python/django/djangostart.bat
+++ Python/django/djangostart.bat
@@ -1,5 +1,35 @@
 
 set /p project="Enter name of project:"
 
+
+
+django-admin startproject %project%
+timeout 1
+cd %project%
+set /p app_name="Enter name of app:"
+mkdir apps
+cd apps
+null> __init__.py
+python ..\manage.py startapp %app_name%
+timeout 1
+cd %app_name%
+mkdir static
+mkdir templates
+cd static
+mkdir %app_name%
+mkdir css
+mkdir images
+mkddir js
+cd..
+cd templates
+mkdir %app_name%
+cd %app_name%
+null> index.html
+cd..
+cd..
+null> ulrs.py
+
 echo "success"
-echo %project%
+echo "created " %project% "and " %app_name%
+echo "Press any key to exit"
+pause

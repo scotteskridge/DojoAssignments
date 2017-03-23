55e653dafcda20ba7355484f28f31144ae7e8f17
Scott Eskridge <scott.eskridge@gmail.com>
2017-03-22 17:53:14 -0700

    relocate to home

--- Python/django/login_and_registration/apps/login/models.py
+++ Python/django/login_and_registration/apps/login/models.py
@@ -3,11 +3,12 @@ from django.db import models
 import re
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
-class UserValidate(models.Manager):
-    def register(self, postData):
+class UserManager(models.Manager):
+    def register_user(self, postData):
+
         errors = [] #make a list of strings and append erros
         reply_to_veiws = {} # make an object to give a status and user_id back
-    #put in valdatations can I validate for both login and register i don;t think so so I'm going to make two methods but thats ok becuase my .Models pulls from the object?
+        #put in valdatations can I validate for both login and register i don;t think so so I'm going to make two methods but thats ok becuase my .Models pulls from the object?
         #check if firstname >2
         #check if last_name >2
         #check if email matches regex and is not empty
@@ -22,7 +23,7 @@ class UserValidate(models.Manager):
             #push the user and a status true back to veiws
 
 
-    pass
+
 
     def login(self, postData):
     #put in valdatations can I validate for both login and register i don;t think so so I'm going to make two methods but thats ok becuase my .Models pulls from the object?
@@ -30,7 +31,7 @@ class UserValidate(models.Manager):
         #check if last_name >2
         #check if email matches regex
         #
-    pass
+
 
 # Create your models here.
 class User(model.Models):
@@ -40,4 +41,4 @@ class User(model.Models):
     password = models.CharField(max_length = 255)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
-    object = UserValidate()
+    objects = UserManager()
